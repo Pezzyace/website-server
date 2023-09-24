83e8ac52e4d14a3d75db1af6041268e09813f7c2
Akaehomen peace <peacedavid049@gmail.com>
2023-09-24 03:18:43 +0100

    commit

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-alert();
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const loginBtn = document.querySelector('.login-btn');
+const closeBtn = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', () => {
+  wrapper.classList.add('active');
+});
+
+
+loginLink.addEventListener('click', () => {
+  wrapper.classList.remove('active');
+})
+
+loginBtn.addEventListener('click', () => {
+  wrapper.classList.add('active-popup');
+});
+
+closeBtn.addEventListener('click', () => {
+  wrapper.classList.remove('active-popup');
+});
+
+
+
+
